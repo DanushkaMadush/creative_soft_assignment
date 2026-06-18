f30ef995e5e0c4c370e017178f1554ca9651d218
danushka <danmadushzxc@gmail.com>
2026-06-18 22:20:52 +0530

    feat:db conn registered

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FarmsDbConnection")));
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
