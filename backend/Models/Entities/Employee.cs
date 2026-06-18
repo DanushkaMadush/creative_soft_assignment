a26bd344f22c48bb6eae359ac8831de9c4a48c9c
danushka <danmadushzxc@gmail.com>
2026-06-18 21:59:24 +0530

    feat:added models

---
+++ backend/Models/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace backend.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public int Age { get; set; }
+        public DateOnly CertifiedUntil { get; set; }       
+        public Guid FishFarmId { get; set; }
+        public FishFarm FishFarm { get; set; } = null!;
+        public Guid RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+        public ICollection<EmployeeImage> Images { get; set; } = new List<EmployeeImage>();
+    }
+}
---
+++ backend/Models/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Models.Entities
+{
+    public class Role
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public required string CreatedBy { get; set; }
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
