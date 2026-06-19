14049496788fe368aa0a71176c18c25882b93580
danushka <danmadushzxc@gmail.com>
2026-06-19 07:45:49 +0530

    feat:added fish farm image upload services

---
+++ backend/Services/Interfaces/IImageUploadService.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Services.Interfaces
+{
+    public interface IImageUploadService
+    {
+        Task<string> SaveFishFarmImageAsync(IFormFile file);
+        void DeleteFishFarmImage(string fileName);
+    }
+}
