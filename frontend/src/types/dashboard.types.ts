86708faee8900fd35f866e11f6bdaec8f015f0cb
danushka <danmadushzxc@gmail.com>
2026-06-22 11:35:00 +0530

    feat:dashoard apis imnegrated

---
+++ frontend/src/types/dashboard.types.ts
@@ -0,0 +1,10 @@
+export interface DashboardTotal {
+  total: number;
+}
+
+export interface FishFarmLocation {
+  fishFarmId: string;
+  fishFarmName: string;
+  latitude: number;
+  longitude: number;
+}
